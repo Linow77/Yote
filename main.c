05bfb411f1566f811ad4bea07ada7602a52b1bb2
Poulpy <paul.repain@yahoo.com>
2019-11-25 15:41:02 +0100

    VerifCaseVide + VerifPionsSurPlateau (vérifie qu'un joueur humain ou démon a des pièces sur le plateau)

--- main.c
+++ main.c
@@ -31,15 +31,12 @@ typedef struct Move {
     Case nouvelle_position;
 } Move;
 
-
 void TireAuSortJoueur(TypeContents *joueur);
 void InitPlateau();
-
-
-
+int VerifCaseVide(Case c);
+int VerifPionsSurPlateau(TypeContents joueur);
 
 TypeContents plateau[5][6];
-
 
 int main()
 {
@@ -53,6 +50,7 @@ int main()
     return 0;
 }
 
+/* Initialise les cases du plateau à VIDE */
 void InitPlateau() {
     int i, j;
 
@@ -63,6 +61,25 @@ void InitPlateau() {
     }
 }
 
+int VerifCaseVide(Case c) {
+    return plateau[c.y][c.x] == VIDE;
+}
+
+/* On vérifie que le joueur a des pions sur le plateau */
+int VerifPionsSurPlateau(TypeContents joueur) {
+    int i, j;
+
+    for (i = 0; i != 6; i++) {
+        for (j = 0; j != 5; j++) {
+            if (plateau[i][j] == joueur)
+                return 0;// 0 == vrai
+        }
+    }
+
+    return 1;
+}
+
+/* Au début du jeu le 1er joueur est tiré au sort */
 void TireAuSortJoueur(TypeContents *joueur) {
     if (rand() % 2)
         *joueur = HOMME;
